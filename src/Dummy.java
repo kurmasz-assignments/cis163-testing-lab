23457a8b09f948e329d291ca03928bfd7d8360fd
Zachary Kurmas <kurmasz@gvsu.edu>
2020-05-07 09:50:51 -0400

    Added a real file

---
+++ src/Dummy.java
@@ -0,0 +1,8 @@
+
+// This is just a Dummy class to help set up an IntelliJ project
+// for Lab 1.  If you aren't using IntelliJ, don't worry about this.
+public class Dummy {
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
